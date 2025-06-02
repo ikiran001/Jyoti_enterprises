a9bafc3cbda4311aa73a49ed2e434dd2598a64a2
Kiran Jadhav <kiran.jadhav@newfold.com>
2025-06-02 13:57:37 +0530

    admin php changes0.1

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ if (isset($_POST['password']) && $_POST['password'] === 'K@9833514014j') {
   die("Wrong password!");
 }
 
-$conn = new mysqli("localhost", "KiranJ", "K@9833514014j", "jyotiffj_jyoti_Enterprises");
+$conn = new mysqli("localhost", "jyotiffj_KiranJ", "K@9833514014j", "jyotiffj_jyoti_Enterprises");
 if ($conn->connect_error) die("DB error");
 
 $result = $conn->query("SELECT * FROM enquiries ORDER BY created_at DESC");
